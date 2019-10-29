343df994a0c6839b27cf67142e4e9b85281e5f53
Steve Golden <sgolden2525@outlook.com>
2019-10-29 01:28:18 -0500

    Refactor componentize
    Added components folder
    Added App.js into components folder
    Added ChatWindow.js into components folder

---
+++ src/components/ChatWindow.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class ChatWindow extends Component {
+  constructor(props) {
+    super(props)
+
+    this.isDisabled = this.isDisabled.bind(this);
+  }
+
+  isDisabled = () => {
+    console.log("Check disabled")
+    return false;
+  };
+
+  render() {
+    console.log(this.props.messages)
+    return (
+      
+        <div className="chat-window">
+          <h2>Super Awesome Chat</h2>
+          <div className="name sender">{this.props.users[this.props.index].username}</div>
+
+          <ul className="message-list">
+            {this.props.messages.map((message, index) => (
+              <li
+                key={index}
+                className={
+                  message.username === this.props.users[this.props.index].username ? 'message sender' : 'message recipient'
+                }
+              >
+                <p>{`${message.username}: ${message.text}`}</p>
+              </li>
+            ))}
+          </ul>
+
+          <div>
+            <form className="input-group">
+              <input type="text" className="form-control" placeholder="Enter your message..." />
+              <div className="input-group-append">
+                <button className="btn submit-button" disabled={this.isDisabled()}>
+                  SEND
+                  </button>
+              </div>
+            </form>
+          </div>
+        </div>
+    )
+  }
+}
+
+export default ChatWindow
